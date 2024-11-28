2491d6b0efea0634554e037ebab1720b40964a96
lunatictiol <bhatsabiq9@gmail.com>
2024-11-28 14:27:57 +0530

    cors changes

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,18 @@ const server = express();
 const port = process.env.PORT || 3000;
 server.use(express.json());
 
-server.use("/api/v1/task",auth,taskRouter)
-server.use("/api/v1/user",userRouter)
 const corsOptions = {
-    origin: 'http://localhost:5173/', // use your actual domain name (or localhost), using * is not recommended
+    origin: 'http://localhost:5173/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'application/json', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
   };
   
 server.use(cors(corsOptions));
+
+server.use("/api/v1/task",auth,taskRouter)
+server.use("/api/v1/user",userRouter)
+
 
 connectDb()
 
