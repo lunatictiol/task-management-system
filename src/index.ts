977ffb7181297bffa5ddea76893d9f9c2a9d4495
lunatictiol <bhatsabiq9@gmail.com>
2024-11-28 21:23:15 +0530

    cors deployed

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import taskRouter from './controller/TaskController/Task.Routes';
 import { connectDb } from './configs/mongoose.config';
 import { auth } from './middleware/autth.middleware';
 import userRouter from './controller/UserController/User.Routes';
-
+import cors from "cors";
 dotenv.config();
 const server = express();
 const port = process.env.PORT || 3000;
 server.use(express.json());
 
-
+const corsOptions = {
+    origin: 'https://task-management-system-six-pied.vercel.app/',  
+  };
+  
+server.use(cors(corsOptions));
 
 server.use("/api/v1/task",auth,taskRouter)
 server.use("/api/v1/user",userRouter)
