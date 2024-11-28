a7a8c61d87429d29d38e48aa0f5e5a63a21db886
lunatictiol <bhatsabiq9@gmail.com>
2024-11-28 13:37:34 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import taskRouter from './controller/TaskController/Task.Routes';
 import { connectDb } from './configs/mongoose.config';
 import { auth } from './middleware/autth.middleware';
 import userRouter from './controller/UserController/User.Routes';
+
 dotenv.config();
 const server = express();
 const port = process.env.PORT || 3000;
@@ -12,9 +13,11 @@ server.use(express.json());
 server.use("/api/v1/task",auth,taskRouter)
 server.use("/api/v1/user",userRouter)
 
+  
 connectDb()
 
 server.listen(port, () => {
     
     console.log(`Server running at http://localhost:${port}`);
 });
+
